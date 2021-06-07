4f43395f36af86587bb46540e063e08333f43079
pssilva22 <pedro442.rodrigues@gmail.com>
2021-06-07 01:49:18 -0300

    refactor: refact add to python3 support

--- docs/captive_portal.py
+++ docs/captive_portal.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python
-
 import subprocess
-import http.server
-import socketserver
+import BaseHTTPServer
 import cgi
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # These variables are used as settings
-PORT       = int(os.getenv('PORT')) 
-IFACE      = os.getenv('IFACE')
-IP_ADDRESS = os.getenv('IP_ADDRESS')
+PORT       = 9090         # the port in which the captive portal web server listens 
+IFACE      = "wlan2"      # the interface that captive portal protects
+IP_ADDRESS = "172.16.0.1" # the ip address of the captive portal (it can be the IP of IFACE) 
 
 '''
 This it the http server used by the the captive portal
 '''
-class CaptivePortal(http.server.SimpleHTTPRequestHandler):
+class CaptivePortal(BaseHTTPServer.BaseHTTPRequestHandler):
     #this is the index of the captive portal
     #it simply redirects the user to the to login page
     html_redirect = """
@@ -55,9 +48,9 @@ class CaptivePortal(http.server.SimpleHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         if path == "/login":
-            self.wfile.write(self.html_login.encode())
+            self.wfile.write(self.html_login)
         else:
-            self.wfile.write(self.html_redirect.encode())
+            self.wfile.write(self.html_redirect)
     '''
     this is called when the user submits the login form
     '''
@@ -77,34 +70,37 @@ class CaptivePortal(http.server.SimpleHTTPRequestHandler):
         if username == 'nikos' and password == 'fotiou':
             #authorized user
             remote_IP = self.client_address[0]
-            print('New authorization from ', remote_IP)
-            print('Updating IP tables')
+            print 'New authorization from '+ remote_IP
+            print 'Updating IP tables'
             subprocess.call(["iptables","-t", "nat", "-I", "PREROUTING","1", "-s", remote_IP, "-j" ,"ACCEPT"])
             subprocess.call(["iptables", "-I", "FORWARD", "-s", remote_IP, "-j" ,"ACCEPT"])
-            self.wfile.write("You are now authorized. Navigate to any URL".encode())
+            self.wfile.write("You are now authorized. Navigate to any URL")
         else:
             #show the login form
-            self.wfile.write(self.html_login.encode())
+            self.wfile.write(self.html_login)
+        
+    #the following function makes server produce no output
+    #comment it out if you want to print diagnostic messages
+    #def log_message(self, format, *args):
+    #    return
 
-print("*********************************************")
-print("* Note, if there are already iptables rules *")
-print("* this script may not work. Flush iptables  *")
-print("* at your own risk using iptables -F        *")
-print("*********************************************")
-print("Updating iptables")
-print(".. Allow TCP DNS")
+print "*********************************************"
+print "* Note, if there are already iptables rules *"
+print "* this script may not work. Flush iptables  *"
+print "* at your own risk using iptables -F        *"
+print "*********************************************"
+print "Updating iptables"
+print ".. Allow TCP DNS"
 subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", "53", "-j" ,"ACCEPT"])
-print(".. Allow UDP DNS")
+print ".. Allow UDP DNS"
 subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "udp", "--dport", "53", "-j" ,"ACCEPT"])
-print(".. Allow traffic to captive portal")
+print ".. Allow traffic to captive portal"
 subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-p", "tcp", "--dport", str(PORT),"-d", IP_ADDRESS, "-j" ,"ACCEPT"])
-print(".. Block all other traffic")
+print ".. Block all other traffic"
 subprocess.call(["iptables", "-A", "FORWARD", "-i", IFACE, "-j" ,"DROP"])
-print("Starting web server")
-
-httpd = socketserver.TCPServer(((IP_ADDRESS, PORT)), CaptivePortal)
-
-print("Redirecting HTTP traffic to captive portal")
+print "Starting web server"
+httpd = BaseHTTPServer.HTTPServer(('', PORT), CaptivePortal)
+print "Redirecting HTTP traffic to captive portal"
 subprocess.call(["iptables", "-t", "nat", "-A", "PREROUTING", "-i", IFACE, "-p", "tcp", "--dport", "80", "-j" ,"DNAT", "--to-destination", IP_ADDRESS+":"+str(PORT)])
 
 try:
